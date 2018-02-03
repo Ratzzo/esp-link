f2073032ebca81edbd1887af53bc7a22ad8448f8
Ratzzo <ignacio.4096@gmail.com>
2018-02-03 01:02:10 -0600

    updated makefile to depend on environment.mk

--- environment.mk
+++ environment.mk
@@ -1,10 +1,10 @@
-#this file defines the environment, assuming the esp open sdk root is in the parent directory.
+#this file defines the environment, assuming the esp open sdk root is the parent directory.
 
-ESP_OPEN_SDK_ROOT=.. #change this to either an absolute path 
+ESP_OPEN_SDK_ROOT=.. #change this to either an absolute path or a relative path
 
 ESPTOOL_BAUD=921600 
-OPEN_SDK_BASE=$(shell cd $(ESP_OPEN_SDK_ROOT);pwd;) #get parent dir absolute path
+OPEN_SDK_BASE=$(shell cd $(ESP_OPEN_SDK_ROOT);pwd;) #get the absolute path of ESP_OPEN_SDK_ROOT
 SDK_BASE=$(strip $(OPEN_SDK_BASE))/sdk
 ESPTOOL=$(strip $(OPEN_SDK_BASE))/esptool/esptool-cb.py #esptool-cb is a modified esptool where the env #! was changed to python2
-export PATH := $(strip $(OPEN_SDK_BASE))/binfixes:$(strip $(OPEN_SDK_BASE))/xtensa-lx106-elf/bin:$(PATH) #shove binary into PATH
+export PATH := $(strip $(OPEN_SDK_BASE))/binfixes:$(strip $(OPEN_SDK_BASE))/xtensa-lx106-elf/bin:$(PATH) #shove binary paths into PATH
 
