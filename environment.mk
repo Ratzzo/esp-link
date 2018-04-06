d5d5539bb684fca4c094a8f9a5c0085759d7cd8f
Ratzzo <ignacio.4096@gmail.com>
2018-04-05 20:15:44 -0500

    fixed problem with path change in environment.mk

--- environment.mk
+++ environment.mk
@@ -4,7 +4,8 @@ ESP_OPEN_SDK_ROOT=.. #change this to either an absolute path or a relative path
 
 ESPTOOL_BAUD=921600 
 OPEN_SDK_BASE=$(shell cd $(ESP_OPEN_SDK_ROOT);pwd;) #get the absolute path of ESP_OPEN_SDK_ROOT
+XTENSA_TOOLS_ROOT=$(strip $(ESP_OPEN_SDK_ROOT))xtensa-lx106-elf/bin/
 SDK_BASE=$(strip $(OPEN_SDK_BASE))/sdk
 ESPTOOL=$(strip $(OPEN_SDK_BASE))/esptool/esptool-cb.py #esptool-cb is a modified esptool where the env #! was changed to python2
-export PATH := $(strip $(OPEN_SDK_BASE))/binfixes:$(strip $(OPEN_SDK_BASE))/xtensa-lx106-elf/bin:$(PATH) #shove binary paths into PATH
+export PATH := $(strip $(OPEN_SDK_BASE))/binfixes:$(strip $(XTENSA_TOOLS_ROOT)):$(PATH) #shove binary paths into PATH
 
